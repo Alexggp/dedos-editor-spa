26cef690e92bf93795fa5fdad903b827a519a9e7
Alejandro GGP <alejandroggp@ext.inditex.com>
2022-11-25 08:53:33 +0100

    Sign Up form

---
+++ src/components/LoginFormGroup/SignUpForm/SignUpForm.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+const ProjectForm = ({
+  formData,
+  setFormData,
+  send,
+  login
+}) => {
+
+  const acceptHandler = ()=>{
+    if(!formData.email) {
+      setFormData({...formData, error:true})
+      return;
+    } 
+    send();
+  }
+
+  return(
+
+    <Box
+      component="form"
+      autoComplete='off'
+      sx={{
+        width: 320,
+        padding: 5,
+        backgroundColor: 'white',        
+        '& .MuiTextField-root': { m: 1, width: '300px' },
+      }}
+    >
+      <Typography 
+        sx={{
+          marginBottom: 3,
+        }}
+        color="black"
+        align="center"
+        variant="h4"
+        component="div">
+          {"Sign Up"}
+      </Typography>
+      <TextField
+        required
+        fullWidth
+        autoComplete='off'
+        error = {formData.error}
+        id="userName"
+        label="Nombre"
+        type="text"
+        value={formData.name}
+        onChange={(e)=>setFormData({...formData, name:e.currentTarget.value})}
+      />
+      <TextField
+        required
+        fullWidth
+        autoComplete='off'
+        error = {formData.error}
+        id="userEmail"
+        label="Email"
+        type="email"
+        value={formData.email}
+        onChange={(e)=>setFormData({...formData, email:e.currentTarget.value})}
+      />
+      <TextField
+        required
+        fullWidth
+        autoComplete='off'
+        error = {formData.error}
+        id="userPassword"
+        label="Contraseña"
+        type="password"
+        value={formData.password}
+        onChange={(e)=>setFormData({...formData, password:e.currentTarget.value})}
+      />
+
+
+      <Stack 
+        sx={{
+          marginTop: 5,
+        }}
+        spacing={2} 
+        direction="row" 
+        justifyContent={"space-between"}>
+          <Button variant="outlined" onClick={login}>LogIn</Button>
+          <Button variant="contained" onClick={send}>Aceptar</Button>
+      </Stack>
+    </Box>
+  )
+}
+
+
+export default ProjectForm;
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import LoginForm from '../../components/LoginForm/LoginForm';
+import LoginForm from '../../components/LoginFormGroup/LoginForm/LoginForm';
+import SignUpForm from '../../components/LoginFormGroup/SignUpForm/SignUpForm';
 import classes from './LoginPage.module.css';
 
 
@@ -13,6 +14,13 @@ const LoginPage = () => {
     error: false
   }
   const [formData, setFormData] = useState(emptyForm);
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleIsLogin = (bool) =>{
+    setFormData(emptyForm);
+    setIsLogin(bool);
+  }
+
   const sendHandler = () => {
     console.log(formData)
   }
@@ -20,7 +28,11 @@ const LoginPage = () => {
 
   return (
     <div className={classes.LoginPage}> 
-      <LoginForm formData={formData} setFormData={setFormData} send={sendHandler}/>
+      {isLogin ? 
+        <LoginForm formData={formData} setFormData={setFormData} send={sendHandler} signUp={()=>toggleIsLogin(false)}/>
+      : 
+        <SignUpForm formData={formData} setFormData={setFormData} send={sendHandler} login={()=>toggleIsLogin(true)}/>
+      }
     </div>
   );
 }
