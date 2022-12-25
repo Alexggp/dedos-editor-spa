3f3325a66c35e0be105c2747a21350888145c6cc
Alejandro GGP <alejandroggp@ext.inditex.com>
2022-12-25 15:26:54 +0100

    deleting arrow clicking on trash

--- src/components/Editor/Activity/Trash/Trash.js
+++ src/components/Editor/Activity/Trash/Trash.js
@@ -5,12 +5,21 @@ import classes from './Trash.module.css';
 import Droppable from '../../../../hoc/Droppable/Droppable';
 import { trashActions } from '../../../../store/reducers/trash';
 import { deleteObjetive } from '../../../../store/actions/objetives';
+import usePairing from '../../../../hooks/usePairing';
 
 const Trash = () => {
   const dispatch = useDispatch();
+  const {pairingId, cancelPairing} = usePairing();
   
   const removeItem = (item) =>{
     dispatch(deleteObjetive(item.id))
+  }
+
+  const handleClick = ()=>{
+    if(pairingId){
+      dispatch(deleteObjetive(pairingId))
+      cancelPairing(false);
+    }
   }
 
   return (
@@ -19,6 +28,7 @@ const Trash = () => {
       accept={['SelectionObj', 'CounterObj', 'TimerObj', 'PairingObj']}
       dropped={removeItem}>
       <div className={classes.Trash}
+        onClick={handleClick}
         onMouseEnter={() => { dispatch(trashActions.toggle(true)) }}
         onMouseLeave={() => { dispatch(trashActions.toggle(false)) }}
       />
