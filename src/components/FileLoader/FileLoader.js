9c00e5ee389098f3042b4032870cef63cf0d2cfa
Alejandro GGP <alejandroggp@ext.inditex.com>
2023-01-04 17:23:08 +0100

    FileLoader unmocked

--- src/components/FileLoader/FileLoader.js
+++ src/components/FileLoader/FileLoader.js
@@ -1,38 +1,60 @@
 import React, { useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
+import dedosInstance from '../../apis/dedosInstance';
 
 import classes from './FileLoader.module.css';
 
 
-export default function FileLoaer({children, itemId, onLoad}) {
-  
+export default function FileLoaer({ children, itemId, onLoad }) {
+
   const [loading, setLoading] = useState(false)
 
-  const loadImage = (file) => {
+  const loadImage = async (file) => {
     if (!file) {
       return;
     }
     setLoading(true);
     //Uploading the file using
-    fetch('https://httpbin.org/post', {
-      method: 'POST',
-      body: file,
-      // 👇 Set headers manually for single file upload
-      headers: {
-        'content-type': file.type,
-        'content-length': `${file.size}`, // 👈 Headers need to be a string
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const image="https://images-na.ssl-images-amazon.com/images/I/71+mDoHG4mL.png";
-        onLoad(image);
-      })
-      .catch((err) => console.error(err))
-      .finally(()=>setLoading(false));
+    // fetch('https://httpbin.org/post', {
+    //   method: 'POST',
+    //   body: file,
+    //   //Set headers manually for single file upload
+    //   headers: {
+    //     'content-type': file.type,
+    //     'content-length': `${file.size}`, //Headers need to be a string
+    //   },
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     const image="https://images-na.ssl-images-amazon.com/images/I/71+mDoHG4mL.png";
+    //     onLoad(image);
+    //   })
+    //   .catch((err) => console.error(err))
+    try {
+      const config = {
+        //Set headers manually for single file upload
+        headers: {
+          'content-type': file.type,
+          'content-length': `${file.size}`, //Headers need to be a string
+        }
+      };
+      const response = await dedosInstance.post(`/files`, file, config);
+      if (response.status !== 200) {
+        throw new Error(`Unexpected API call response with status: ${response.status} - ${response.statusText}`);
+      } else {
+        onLoad(response.data.url);
+        setLoading(false)
+      }
+
+
+    } catch (error) {
+      setLoading(false)
+      return;
+    }
+
   };
 
-  const onChangeHandler = (e)=>{
+  const onChangeHandler = (e) => {
     if (e.target.files) {
       loadImage(e.target.files[0]);
     }
@@ -40,9 +62,9 @@ export default function FileLoaer({children, itemId, onLoad}) {
 
   return (
     <div className={classes.FileLoaer}>
-      <input type="file" id={`file_${itemId}`} style={{display: "none"}} accept="image/png, image/gif, image/jpeg" onChange={onChangeHandler}/>
-      <label htmlFor={`file_${itemId}`} onMouseDown={e=>e.stopPropagation()}>
-      {loading ? <CircularProgress size={30} sx={{ color: 'white.500' }}/> : <div className={classes.InputButton}></div>}
+      <input type="file" id={`file_${itemId}`} style={{ display: "none" }} accept="image/png, image/gif, image/jpeg" onChange={onChangeHandler} />
+      <label htmlFor={`file_${itemId}`} onMouseDown={e => e.stopPropagation()}>
+        {loading ? <CircularProgress size={30} sx={{ color: 'white.500' }} /> : <div className={classes.InputButton}></div>}
       </label>
     </div>
   );
